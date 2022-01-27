9e98afbd0fb4f3630d894dcd0a550cf8d9ee5902
Carlos-Padron96 <cpadron96@yahoo.com>
2022-01-26 19:33:25 -0800

    changes added ugly changes

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let body = document.querySelector('body')
+body.style.fontFamily = 'Comic Sans'
+body.style.backgroundColor = 'Green'
+body.style.color = 'Red'
+let header = document.querySelector('#page-header')
+header.style.backgroundColor = 'Orange'
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
 
 // Select some elements...
